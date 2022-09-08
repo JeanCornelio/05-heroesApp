d136b956bc67dfa820a3538c53fd85c84db6da66
JeanCornelio <jcornelioweb@gmail.com>
2022-09-07 23:12:31 -0400

    formulario agregar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,6 +9,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input'
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSelectModule} from '@angular/material/select';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 @NgModule({
@@ -16,6 +17,7 @@ import {MatListModule} from '@angular/material/list';
     MatAutocompleteModule,
     MatProgressSpinnerModule,
     MatSidenavModule,
+    MatSelectModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
